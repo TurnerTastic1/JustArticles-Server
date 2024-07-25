d57bb6c869309f20e4932b95d4148fb1251ca7e4
Turner Naef <Turner.Naef@gmail.com>
2024-07-25 10:25:05 -0400

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use axum::{routing::{get}, Router};
+use log::{debug, warn};
 
 #[tokio::main]
 async fn main() {
+    init_logging();
+
+    debug!("Starting server...");
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app.into_make_service()).await.unwrap();
+}
+
+pub fn init_logging() {
+    if let Err(e) = log4rs::init_file("log4rs.yaml", Default::default()) {
+        warn!("Failed to initialize logging: {:?}", e);
+    }
 }
